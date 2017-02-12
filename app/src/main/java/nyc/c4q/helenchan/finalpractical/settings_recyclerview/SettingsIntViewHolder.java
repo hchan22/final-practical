4d32e4b266943f5919ec551417303ec0600059a7
Helen Chan <hchan22@gmail.com>
2017-02-12 17:49:34 -0500

    mod

--- app/src/main/java/nyc/c4q/helenchan/finalpractical/settings_recyclerview/SettingsIntViewHolder.java
+++ app/src/main/java/nyc/c4q/helenchan/finalpractical/settings_recyclerview/SettingsIntViewHolder.java
@@ -18,13 +18,7 @@ public class SettingsIntViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void bind(Integer position) {
-        String currentString = intNumTextView.getText().toString();
-        currentString = currentString.substring(0 , currentString.length() -2);
-        StringBuilder builder = new StringBuilder();
-        builder.append(currentString);
-        builder.append(position.toString());
-
-        intNumTextView.setText(builder.toString());
+        intNumTextView.setText("Settings - " + position);
     }
 
 
